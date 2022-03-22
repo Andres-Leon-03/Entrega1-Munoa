bccc77fd5945c87bbe8e2b9c1952e658a39b068c
Andres Muñoa Leon <cmunoa@medialabla.com>
2022-03-21 23:54:40 -0500

    falta completar

--- miproyecto/views.py
+++ miproyecto/views.py
@@ -33,6 +33,18 @@ def numero_del_usuario(request,numero):
 def mi_plantilla(request):
     plantilla = open(r"C:\Users\andre\Desktop\miproyecto\miproyecto\plantillas\mi_plantilla.html")
     template = Template(plantilla.read())
-    context = Context()
+    
+    nombre="Andres"
+    apellido="Muñoa"
+    lista=[5,6,4,7,5525,4]
+    
+    diccionario_de_datos={
+        'nombre': nombre,
+        'apellido': apellido,
+        'nombre_largo':len(nombre),
+        'lista':lista
+    }
+    
+    context = Context(diccionario_de_datos)
     plantilla_preparada = template.render(context)
     return HttpResponse(plantilla_preparada)
