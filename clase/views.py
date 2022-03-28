f81c596c75a6200c394113e8802e09c77f188fb2
Andres Muñoa Leon <cmunoa@medialabla.com>
2022-03-28 17:37:33 -0500

    formulario

--- clase/views.py
+++ clase/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render
 from clase.models import Curso
 import random
@@ -11,3 +12,14 @@ def nuevo_curso(request):
     nuevo_curso = Curso(nombre='Curso Python', camada=camada)
     nuevo_curso.save()
     return HttpResponse(f"Se creo el curso {nuevo_curso.nombre} camada {nuevo_curso.camada}")
+
+def formulario_curso(request):
+    
+    print(request.method)
+    if request.method == 'POST':
+        print(request.POST)
+        nuevo_curso = Curso(nombre=request.POST['curso'], camada=request.POST['camada'])
+        nuevo_curso.save()
+        return render(request, 'clase/formulario_curso.html',{'nuevo_curso':nuevo_curso})
+    
+    return render(request, 'clase/formulario_curso.html',{})
--- indice/urls.py
+++ indice/urls.py
@@ -2,12 +2,12 @@ from .views import inicio,otra_vista,numero_random,numero_del_usuario,mi_plantil
 from django.urls import path
    
 urlpatterns=[
-    path('', inicio),
-    path('otra-vista', otra_vista),
+    path('', inicio, name="inicio"),
+    path('otra-vista', otra_vista, name="otra_vista"),
     path('numero-random/', numero_random),
     path('dame-numero/<numero>', numero_del_usuario),
     # path('dame-numero/<int:numero>', numero_del_usuario), CON EL INT ASEGURAMOS QUE ES ENTERO
-    path('mi-plantilla', mi_plantilla),
+    path('mi-plantilla', mi_plantilla, name="mi_plantilla"),
     
     
 ] 
